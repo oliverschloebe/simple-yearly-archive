4e5bbe3808750689568e59567d97d1fb969d0460
Alphawolf <Alphawolf@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-02-22 22:35:13 +0000

    Version update 1.1.10
    git-svn-id: https://plugins.svn.wordpress.org/simple-yearly-archive/trunk@96696 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- js/os_authorplugins_script.js
+++ js/os_authorplugins_script.js
@@ -7,7 +7,7 @@ jQuery("#authorplugins-start").click(function() {
   		url: 'http://extend.schloebe.de/format/json/wordpress/',
   		success: function (j) {
 			jQuery.each(j.plugins, function(i,plugin) {
-				jQuery('#authorpluginsul').append( '<li><a href="' + plugin.os_script_info_url + '" target="_blank"><span class="post">' + plugin.os_script_title + '</span><span class="hidden"> - </span><cite>version ' + plugin.os_script_version + '</cite></a></li>' ).css("display", "none").fadeIn("slow");
+				jQuery('#authorpluginsul').append( '<li><a href="http://extend.schloebe.de/incoming/' + plugin.uid + '/" target="_blank"><span class="post">' + plugin.os_script_title + '</span><span class="hidden"> - </span><cite>version ' + plugin.os_script_version + '</cite></a></li>' ).css("display", "none").fadeIn("slow");
     		});
 		}
 	});
