6a24400620f38a62b97e6c420ae161f9f44009f1
Alphawolf <Alphawolf@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-08-06 17:15:26 +0000

    Version 1.3 update * New plugin maintainer. Hello. :) Name's Oliver. * Code rewrite and cleanup
    git-svn-id: https://plugins.svn.wordpress.org/disable-wordpress-updates/trunk@752416 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- disable-updates.php
+++ disable-updates.php
@@ -1,62 +1,171 @@
 <?php
+/**
+ * The main plugin file
+ *
+ * @package WordPress_Plugins
+ * @subpackage OS_Disable_WordPress_Updates
+ */
+
 /*
-Plugin Name: Disable WordPress Updates
+Plugin Name: Disable All WordPress Updates
 Description: Disables the theme, plugin and core update checking, the related cronjobs and notification system.
 Plugin URI:  http://wordpress.org/plugins/disable-wordpress-updates/
-Version:     1.2
+Version:     1.3
 Author:      Oliver Schlöbe, originally developed by Tanja Preu&szlig;e 
 Author URI:  http://www.schloebe.de/
 
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
+Copyright 2013 Oliver Schlöbe (email : scripts@schloebe.de)
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
 
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
-//Disable Theme Updates
-# 2.8 to 3.0:
-remove_action( 'load-themes.php', 'wp_update_themes' );
-remove_action( 'load-update.php', 'wp_update_themes' );
-remove_action( 'admin_init', '_maybe_update_themes' );
-remove_action( 'wp_update_themes', 'wp_update_themes' );
-add_filter( 'pre_transient_update_themes', create_function( '$a', "return null;" ) );
-wp_clear_scheduled_hook( 'wp_update_themes' );
-
-# 3.0:
-remove_action( 'load-update-core.php', 'wp_update_themes' );
-add_filter( 'pre_site_transient_update_themes', create_function( '$a', "return null;" ) );
-wp_clear_scheduled_hook( 'wp_update_themes' );
 
 
-//Disable Plugin Updates
-# 2.8 to 3.0:
-remove_action( 'load-plugins.php', 'wp_update_plugins' );
-remove_action( 'load-update.php', 'wp_update_plugins' );
-remove_action( 'admin_init', '_maybe_update_plugins' );
-remove_action( 'wp_update_plugins', 'wp_update_plugins' );
-add_filter( 'pre_transient_update_plugins', create_function( '$a', "return null;" ) );
-wp_clear_scheduled_hook( 'wp_update_plugins' );
-
-# 3.0:
-remove_action( 'load-update-core.php', 'wp_update_plugins' );
-add_filter( 'pre_site_transient_update_plugins', create_function( '$a', "return null;" ) );
-wp_clear_scheduled_hook( 'wp_update_plugins' );
+/**
+ * Define the plugin version
+ */
+define("OSDWPUVERSION", "1.3");
 
 
-//Diasable Core Updates
-# 2.8 to 3.0:
-remove_action( 'wp_version_check', 'wp_version_check' );
-remove_action( 'admin_init', '_maybe_update_core' );
-add_filter( 'pre_transient_update_core', create_function( '$a', "return null;" ) );
-wp_clear_scheduled_hook( 'wp_version_check' );
+/**
+ * The OS_Disable_WordPress_Updates class
+ *
+ * @package 	WordPress_Plugins
+ * @subpackage 	OS_Disable_WordPress_Updates
+ * @since 		1.3
+ * @author 		scripts@schloebe.de
+ */
+class OS_Disable_WordPress_Updates {
+	/**
+	 * The OS_Disable_WordPress_Updates class constructor
+	 * initializing required stuff for the plugin
+	 *
+	 * PHP 4 Compatible Constructor
+	 *
+	 * @since 		1.3
+	 * @author 		scripts@schloebe.de
+	 */
+	function OS_Disable_WordPress_Updates() {
+		$this->__construct();
+	}
+	
+	
+	/**
+	 * The OS_Disable_WordPress_Updates class constructor
+	 * initializing required stuff for the plugin
+	 *
+	 * PHP 5 Constructor
+	 *
+	 * @since 		1.3
+	 * @author 		scripts@schloebe.de
+	 */
+	function __construct() {
+		add_action('admin_init', array(&$this, 'admin_init'));
+		
+		/*
+		 * Disable Theme Updates
+		 * 2.8 to 3.0
+		 */
+		add_filter( 'pre_transient_update_themes', create_function( '$a', "return null;" ) );
+		/*
+		 * 3.0
+		 */
+		add_filter( 'pre_site_transient_update_themes', create_function( '$a', "return null;" ) );
+		
+		/*
+		 * Disable Plugin Updates
+		 * 2.8 to 3.0
+		 */
+		add_action( 'pre_transient_update_plugins', array(&$this, create_function( '$a', "return null;" )) );
+		/*
+		 * 3.0
+		 */
+		add_filter( 'pre_site_transient_update_plugins', create_function( '$a', "return null;" ) );
+		
+		/*
+		 * Disable Core Updates
+		 * 2.8 to 3.0
+		 */
+		add_filter( 'pre_transient_update_core', create_function( '$a', "return null;" ) );
+		/*
+		 * 3.0
+		 */
+		add_filter( 'pre_site_transient_update_core', create_function( '$a', "return null;" ) );
+	}
+	
+	
+	/**
+	 * Initialize and load the plugin stuff
+	 *
+	 * @since 		1.3
+	 * @author 		scripts@schloebe.de
+	 */
+	function admin_init() {
+		if ( !function_exists("remove_action") ) return;
+	
+		/*
+		 * Disable Theme Updates
+		 * 2.8 to 3.0
+		 */
+		remove_action( 'load-themes.php', 'wp_update_themes' );
+		remove_action( 'load-update.php', 'wp_update_themes' );
+		remove_action( 'admin_init', '_maybe_update_themes' );
+		remove_action( 'wp_update_themes', 'wp_update_themes' );
+		wp_clear_scheduled_hook( 'wp_update_themes' );
+		
+		/*
+		 * 3.0
+		 */
+		remove_action( 'load-update-core.php', 'wp_update_themes' );
+		wp_clear_scheduled_hook( 'wp_update_themes' );
+		
+		
+		/*
+		 * Disable Plugin Updates
+		 * 2.8 to 3.0
+		 */
+		remove_action( 'load-plugins.php', 'wp_update_plugins' );
+		remove_action( 'load-update.php', 'wp_update_plugins' );
+		remove_action( 'admin_init', '_maybe_update_plugins' );
+		remove_action( 'wp_update_plugins', 'wp_update_plugins' );
+		wp_clear_scheduled_hook( 'wp_update_plugins' );
+		
+		/*
+		 * 3.0
+		 */
+		remove_action( 'load-update-core.php', 'wp_update_plugins' );
+		wp_clear_scheduled_hook( 'wp_update_plugins' );
+		
+		
+		/*
+		 * Disable Core Updates
+		 * 2.8 to 3.0
+		 */
+		remove_action( 'wp_version_check', 'wp_version_check' );
+		remove_action( 'admin_init', '_maybe_update_core' );
+		wp_clear_scheduled_hook( 'wp_version_check' );
+		
+		
+		/*
+		 * 3.0
+		 */
+		wp_clear_scheduled_hook( 'wp_version_check' );
+	}
+}
 
-# 3.0:
-add_filter( 'pre_site_transient_update_core', create_function( '$a', "return null;" ) );
-wp_clear_scheduled_hook( 'wp_version_check' );
+if ( class_exists('OS_Disable_WordPress_Updates') && is_admin() ) {
+	$OS_Disable_WordPress_Updates = new OS_Disable_WordPress_Updates();
+}
 ?>
--- readme.txt
+++ readme.txt
@@ -1,48 +1,64 @@
-=== Disable WordPress Updates ===
+=== Disable All WordPress Updates ===
 Contributors: Alphawolf
-Donate link: none
+Donate link: http://www.schloebe.de/donate/
 Tags: disable, updates, theme, core
-Requires at least: 2.8
+Requires at least: 3.4
 Tested up to: 3.6
-Stable tag: 1.2
+Stable tag: trunk
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
 Disables the theme, plugin and core update checking, the related cronjobs and notification system.
 
 == Description ==
 
-This Plugin completely disables the theme, plugin and core update checking system in WordPress. The plugin prevents WordPress from 
-checking for updates incl. cronjobs, and prevents any notifications from being displayed.
+This plugin completely disables the theme, plugin and core update checking system in WordPress. The plugin prevents WordPress from 
+checking for updates including cronjobs, and prevents any notifications from being displayed.
 
-This Plugin combines und extends the three plugins of John Blackbourn:
-[Disable Wordpress Theme Update](http://wordpress.org/extend/plugins/disable-wordpress-theme-updates/), 
-[Disable Wordpress Plugin Update](http://wordpress.org/extend/plugins/disable-wordpress-plugin-updates/),
-[Disable Wordpress Core Update](http://wordpress.org/extend/plugins/disable-wordpress-core-update/)
+This plugin combines und extends these three plugins:
+* [Disable Wordpress Theme Update](http://wordpress.org/extend/plugins/disable-wordpress-theme-updates/), 
+* [Disable Wordpress Plugin Update](http://wordpress.org/extend/plugins/disable-wordpress-plugin-updates/),
+* [Disable Wordpress Core Update](http://wordpress.org/extend/plugins/disable-wordpress-core-update/)
 
-It's **very** important that you keep your WordPress theme, core and plugins up to date. If you don't, your blog or website could 
-be susceptible to security vulnerabilities or performance issues. 
+It's *very* important that you keep your WordPress theme, core and plugins up to date. If you don't, your blog or website could 
+be **susceptible to security vulnerabilities** or performance issues.
 
-If you use this plugin, you must make sure you keep yourself up to date with new releases of your active wordpress version, plugins 
+If you use this plugin, make sure you keep yourself up to date with new releases of your active WordPress version, plugins 
 and themes and update them as new versions are released (simply by deactivating this plugin for a short time).
+
+[Developer on Google+](https://plus.google.com/118074611982254715031 "Developer on Google+") | [Developer on Twitter](http://twitter.com/wpseek "Developer on Twitter")
+
+**Looking for more WordPress plugins? Visit [www.schloebe.de/portfolio/](http://www.schloebe.de/portfolio/)**
+
+
+== Frequently Asked Questions ==
+
+= What can I do to be up to date with new releases of my WordPress version, plugins and themes? =
+
+Simply deativate the plugin for a short time. In case new versions are available you see the WordPress notifications.
 
 
 == Installation ==
 
-1. Unzip the ZIP plugin file 
-2. Copy the `disable-wordpress-updates` folder into `wp-content/plugins/` directory
-2. Activate the plugin through the 'Plugins' menu in WordPress.
+1. Download the plugin and unzip it.
+2. Upload the folder disable-wordpress-updates/ to your /wp-content/plugins/ folder.
+3. Activate the plugin from your WordPress admin panel.
+4. Installation finished.
 
 With activating the plugin all theme, core and plugin update checkings are disabled. If you want to have a quick look if new versions 
 are existing, simply deactive this plugin for a short time.
 
 
-== Frequently Asked Questions ==
+== Screenshots ==
 
-= Where can I do to be up to date with new releases of my wordpress version, plugins and themes? =
-
-Simply deativate the plugin for a short time. In case new versions are existing you see the wordpress notifications.
+None. :)
 
 
 == Changelog ==
+
+= 1.3 =
+* New plugin maintainer. Hello. :) Name's Oliver.
+* Code rewrite and cleanup
 
 = 1.2 =
 * Name and URL Update (German plugin description updated)
@@ -52,3 +68,8 @@ Simply deativate the plugin for a short time. In case new versions are existing 
 
 = 1.0 =
 * Initial release
+
+
+== Upgrade Notice ==
+
+New plugin maintainer. Hello. :) Name's Oliver. Plus Code rewrite and cleanup
